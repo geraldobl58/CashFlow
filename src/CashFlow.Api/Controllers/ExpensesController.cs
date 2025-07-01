b206bff6de5d8c7c9357af1e9da93ac35e34de6e
Geraldo Luiz <geraldobl58@gmail.com>
2025-07-01 15:22:19 -0300

    Refactor error handling in ExpensesController
    Removed specific error handling from ExpensesController and added a global ExceptionFilter to manage exceptions across the application. The new filter processes validation and unexpected errors, returning appropriate HTTP responses. Updated Program.cs to register the ExceptionFilter in MVC options for centralized error management.

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,7 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
-using CashFlow.Communication.Responses;
-using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers
@@ -13,26 +11,12 @@ namespace CashFlow.Api.Controllers
         [HttpPost]
         public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
         {
-            try 
-            {
-                var useCase = new RegisterExpenseUseCase();
+            var useCase = new RegisterExpenseUseCase();
 
-                var response = useCase.Execute(request);
+            var response = useCase.Execute(request);
 
-                return Created(string.Empty, response);
-            }
-            catch (ErrorOnValidationException ex)
-            {
-                var errorResponse = new ResponseErrorJson(ex.Errors);
+            return Created(string.Empty, response);
 
-                return BadRequest(errorResponse);
-            }
-            catch
-            {
-                var errorResponse = new ResponseErrorJson("An unexpected error occurred.");
-               
-                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
-            }
         }
     }
 }
